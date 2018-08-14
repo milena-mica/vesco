38171d456820addd6b58be7f1aff3953a2cf9971
milena-mica <trajkovic.milena.mica@gmail.com>
2018-08-14 21:25:04 +0200

    Add Magnify-Gallery Effect using jQuery

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,13 @@ $(function() {
   // animate on scroll
   new WOW().init();
 });
+
+$(function() {
+  $("#work").magnificPopup ({
+     delegate: 'a', // child items selector, by clicking on it popup will open
+     type: 'image',
+     gallery: {
+       enabled: true
+     }
+  });
+});
