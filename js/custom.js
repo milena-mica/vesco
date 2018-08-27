5fa9cecdf8d44c8ecea4627f5c040c1f10082bc0
milena-mica <trajkovic.milena.mica@gmail.com>
2018-08-27 11:57:42 +0200

    Add Smooth Scroll Using jQuery - Navigation

--- js/custom.js
+++ js/custom.js
@@ -70,3 +70,17 @@ $(function() {
     }
   });
 });
+
+
+$(function() {
+  $("a.smooth-scroll").click(function(event) {
+    event.preventDefault();
+
+    // get/return id like #about, #work, #team and etc
+    var section = $(this).attr("href");
+
+    $('html, body').animate({
+       scrollTop: $(section).offset().top - 64
+    }, 1250, "easeInOutExpo");
+  });
+});
