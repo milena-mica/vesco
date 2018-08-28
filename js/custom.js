d7a9109f974d28ca0a583e5328322c6e2ca0b15a
milena-mica <trajkovic.milena.mica@gmail.com>
2018-08-28 11:24:04 +0200

    Add Back To Top Button - Navigation

--- js/custom.js
+++ js/custom.js
@@ -64,9 +64,11 @@ $(function() {
     if($(this).scrollTop() < 50 ) {
        // hide nav
        $("nav").removeClass("vesco-top-nav");
+       $("#back-to-top").fadeOut();
     } else {
        // show nav
        $("nav").addClass("vesco-top-nav");
+       $("#back-to-top").fadeIn();
     }
   });
 });
