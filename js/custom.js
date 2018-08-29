b50201840683b93fe33c4866b303be3b3e354d27
milena-mica <trajkovic.milena.mica@gmail.com>
2018-08-29 20:09:55 +0200

    Small devices-Responsive

--- js/custom.js
+++ js/custom.js
@@ -47,18 +47,37 @@ $(function() {
 });
 
 
-
+/* CLIENTS */
 $(function() {
+
   $("#clients-list").owlCarousel({
     items: 6,
     autoplay: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
+    responsive: {
+      // breakpoint from 0 up
+      0 : {
+        items: 1
+      },
+      // breakpoint from 480 up
+      480 : {
+        items: 3
+      },
+      // breakpoint from 768 up
+      768 : {
+        items: 5
+      },
+      // breakpoint from 992 up
+      992 : {
+        items: 6
+      }
+    }
   });
 });
 
-
+/* NAVIGATION */
 $(function() {
   $(window).scroll(function() {
     if($(this).scrollTop() < 50 ) {
